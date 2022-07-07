9df34ff9f2a2bb005da523247d3dddb242ac5167
Mansi Wakharkar <mansi.wakharkar@beyonmoney.com>
2022-07-07 14:30:35 +0300

    Moving to directory

---
+++ basics.py
@@ -0,0 +1,46 @@
+#Print
+print("Hello, World!")
+
+#Arithmatic
+#Adding number - int() provides type conversion of string to integer
+#a = int(input('Enter number a: '))
+#b = int(input('Enter number b: '))
+#print('Addition is: ', (a+b))
+
+#Adding / concat string
+#a = input('Enter number a: ')
+#b = input('Enter number b: ')
+#print('Addition is: ', (a+b))
+
+print("Python data types:")
+print("Numeric (immutable): Integer, Float, Complex")
+
+#Numerics
+#Numeric immutability - You can overwrite the number, but you can't alter any specific digit by treating number as Arrau
+a = 10
+b = -10
+c = 3.142
+d = 10+3j
+print('a + b = ', (a+b))
+print('b + c  = ', (b+c))
+print('c + d  = ', (c+d))
+print('a * d  = ', (a*d))
+
+#Convert numeric string to binary string
+sstr = "10010"
+snum = int(sstr, 2)
+sfloat = float(sstr)
+print('Binary: ', sstr, ', Int:', snum)
+print('Binary: ', sstr, ', Float:', sfloat)
+
+#Flow Control - Loops - for, while
+for i in range(a):
+    print('In loop: ', i*2)
+
+#Flow Control - Conditional statements - if, else
+if a>10 :
+    print ('a is greater than 10')
+else :
+    print('a is less than or equal to 10')
+
+
