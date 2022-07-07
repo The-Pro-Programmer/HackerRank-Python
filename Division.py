f69a7439ba4fd7a38b576d2fc3de2ba18c32414c
Mansi Wakharkar <mansi.wakharkar@beyonmoney.com>
2022-07-07 13:26:31 +0300

    Arithmatic operators program

---
+++ Division.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    if b == 0:
+        print(int(b))
+        print(float(b))
+    else:
+        print(int(a/b))
+        print(float(a/b))
+
