472f4b52b19acf3d883c9abb89e9349aaaa25a4a
Mansi Wakharkar <mansiswakharkar@gmail.com>
2022-07-15 14:56:03 +0300

    Integer division - Modulo Operator-Divmod function

---
+++ maths/Mod-Divmod.py
@@ -0,0 +1,9 @@
+#https://www.hackerrank.com/challenges/python-mod-divmod/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a%b)
+    print(divmod(a,b))
+
